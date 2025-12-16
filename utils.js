1bc7900da5616e449b817659278bfeb7dceb7e47
Dineth-De-Silva <dineth2022@gmail.com>
2025-12-16 20:58:49 +0530

    Responsive start page styling complete

--- utils.js
+++ utils.js
@@ -1,13 +1,11 @@
 // Normal functions
 // Function to check a subset is a subset of parent set
 function isSubsetOf(parentArr, subsetArr) {
-  // The 'every' method checks if all elements in the subsetArr pass a test.
   return subsetArr.every((element) => {
-    // The test is whether the parentArr includes the current element.
     return parentArr.includes(element);
   });
 }
-
+// Function to generate random integer between min (inclusive) and max (exclusive)
 function getRndInteger(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
+  return Math.floor(Math.random() * (max + 1 - min)) + min;
 }
